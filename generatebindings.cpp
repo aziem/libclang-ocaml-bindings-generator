b2fb6bfdfb197dd2a825eb932268f40fb21a2cc8
Aziem Chawdhary <aziem@chawdhary.co.uk>
2017-11-12 13:48:31 +0000

    Generate struct and func decl bindings

--- generatebindings.cpp
+++ generatebindings.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <tuple>
 
 #include <clang-c/Index.h>
 
@@ -10,6 +11,17 @@ using namespace std;;
 struct EnumInfo {
   string name;
   vector<string> enumconstants;
+};
+
+struct FuncDecl {
+  string name;
+  string resulttype;
+  vector<string> paramtypes;
+};
+
+struct StructDecl {
+  string name;
+  vector<tuple<string, string>> fieldnames;
 };
 
 ostream& operator<<(ostream& stream, const CXString& str) {
@@ -40,6 +52,114 @@ CXTranslationUnit parseFile(CXIndex index, string filename) {
     return unit;
   }
 }
+
+string typeToString(CXType ty) {
+  switch(ty.kind) {
+  case CXType_Void: return "T.void";
+  case CXType_UInt: return "T.size_t";
+  case CXType_Int: return "T.int";
+  case CXType_Bool: return "T.bool";
+  case CXType_Char_S: return "T.string";
+  case CXType_Char_U: return "T.string";
+  //case CXType_Unexposed: return "UNEXPOSED";  <--- This is returned for a function pointer in a struct
+  // Solution here maybe? http://clang-developers.42468.n3.nabble.com/Function-pointer-in-a-struct-declaration-td4044706.html 
+
+  case CXType_Pointer: {
+    CXType ptee = clang_getPointeeType(ty);
+    return "T.ptr " + typeToString(ptee);
+  }
+
+  default: return getFromCXString(clang_getTypeSpelling(ty)); 
+  }
+}
+
+
+CXChildVisitResult gatherStructDecls(CXCursor c, CXCursor parent, CXClientData client_data) {
+  switch(clang_getCursorKind(c)) {
+  case CXCursor_StructDecl: {
+    vector<StructDecl>* v = (vector<StructDecl>*) client_data;
+
+    StructDecl s;
+    s.name = getFromCXString(clang_getCursorSpelling(c));
+
+    clang_visitChildren(c,
+			[](CXCursor c, CXCursor p, CXClientData cd) {
+			  switch(clang_getCursorKind(c)) {
+			  case CXCursor_FieldDecl: {
+			    CXType f_type = clang_getCursorType(c);
+			    vector<tuple<string, string>>* fieldnames = (vector<tuple<string, string>>*) cd;
+			    string fieldname = getFromCXString(clang_getCursorSpelling(c));
+			    fieldnames->push_back(make_tuple(fieldname, typeToString(f_type)));
+			    break;
+			  }
+			  default: break;
+			  }
+			  return CXChildVisit_Recurse;
+			}, (CXClientData) &s.fieldnames);
+    v->push_back(s);
+    break;
+  }
+
+  default: break;
+    
+  }
+
+  return CXChildVisit_Recurse;
+}
+
+void printStructDecl(StructDecl s) {
+  cout << "type " << s.name << endl;
+  cout << "let " << s.name << " : " << s.name << " Ctypes.structure T.typ = T.structure \"" << s.name << "\"" << endl;
+  for( auto f : s.fieldnames ) {
+    cout << "let " << s.name + "_" + get<0>(f) << " = T.field " << s.name << " \"" << get<0>(f) << "\" (" << get<1>(f) << ")" << endl;
+  }
+}
+
+
+CXChildVisitResult gatherFuncDecls(CXCursor c, CXCursor parent, CXClientData client_data) {
+  switch(clang_getCursorKind(c)) {
+  case CXCursor_FunctionDecl: {
+    vector<FuncDecl>* v = (vector<FuncDecl>*) client_data;
+
+    FuncDecl f;
+    CXType fty = clang_getCursorType(c);
+    CXType retty = clang_getResultType(fty);
+
+    f.name= getFromCXString(clang_getCursorSpelling(c));
+    f.resulttype = typeToString(retty);
+
+    clang_visitChildren(c,
+			[](CXCursor c, CXCursor p, CXClientData cd) {
+			  switch(clang_getCursorKind(c)) {
+			  case CXCursor_ParmDecl: {
+			    CXType c_type = clang_getCursorType(c);
+			    vector<string>* paramtypes = (vector<string>*) cd;
+			    string s = typeToString(c_type);
+			    paramtypes->push_back(s);
+			    break;
+			  }
+			  default: break;
+			  }
+			  return CXChildVisit_Recurse;
+			}, (CXClientData) &f.paramtypes);
+    v->push_back(f);
+    break;
+  }
+  default: break;
+    
+  }
+
+  return CXChildVisit_Recurse;
+}
+
+void printFuncDecl(FuncDecl f) {
+  cout << "let " << f.name << " = F.foreign \"" << f.name << "\" (";
+  for(auto e : f.paramtypes) {
+    cout << e << " @-> ";
+  }
+  cout << "returning (" << f.resulttype << "))" << endl;
+}
+
 
 CXChildVisitResult gatherEnumInfo(CXCursor c, CXCursor parent, CXClientData client_data) {
   switch(clang_getCursorKind(c)) {
@@ -97,17 +217,44 @@ void printEnumBindings(EnumInfo e) {
   cout << "]" << endl;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+  if(argc < 2) {
+    cout << "Requires a C/C++ header file to parse\n";
+	    exit(1);
+  }
+
+  
   CXIndex index = clang_createIndex(0,0);
-  auto unit = parseFile(index, "binaryninjacore.h");
+  auto unit = parseFile(index, argv[1]);
 
   CXCursor cur = clang_getTranslationUnitCursor(unit);
 
   vector<EnumInfo> enums;
   clang_visitChildren(cur, gatherEnumInfo, &enums);
 
+  vector<FuncDecl> funcdecls;
+  clang_visitChildren(cur, gatherFuncDecls, &funcdecls);
+
+  vector<StructDecl> structdecls;
+  clang_visitChildren(cur, gatherStructDecls, &structdecls);
+
+
   for(auto e : enums) {
     printEnumBindings(e);
+    cout << endl;
+  }
+
+  cout << "FuncDecls Size: " << funcdecls.size() << endl;
+
+  for (auto f : funcdecls) {
+    printFuncDecl(f);
+    cout << endl;
+  }
+  
+  cout << "StructDecls Size: " << structdecls.size() << endl;
+
+  for(auto s : structdecls) {
+    printStructDecl(s);
     cout << endl;
   }
 
